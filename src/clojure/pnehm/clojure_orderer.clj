647473cfcd14e5e57e245ab0c95d26752e1f69c2
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2009-06-11 22:00:09 +0000

    Tweaks.

--- src/clojure/pnehm/clojure_orderer.clj
+++ src/clojure/pnehm/clojure_orderer.clj
@@ -8,18 +8,13 @@
 (defn -init []
   [[] (atom [])])
 
-(defn count-words
-  [coll]
+
+(defn count-words [coll]
   (reduce #(merge-with + %1 {%2 1}) {} coll))
 
-(defn cmpr [a b]
-  (let [freq (compare (val b) (val a))]
-    (cond (not= freq 0) freq
-      :else (compare (key a) (key b)))))
+(defn cmpr [[val1 freq1] [val2 freq2]]
+  (let [freq (compare freq2 freq1)]
+    (if (not= freq 0) freq (compare val1 val2))))
 
-(defn -orderByFreq [_ arg]
-
-  (map #(key %) (sort cmpr
-    (count-words arg))))
-
-;(println (-orderByFreq :a ["b", "d", "b", "b", "a", "c", "a"]))
+(defn -orderByFreq [_ coll]
+  (if (empty? coll) () (keys (sort cmpr (count-words coll)))))
