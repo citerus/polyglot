a96a491fa3b752d9e350d0b09f3e59b379a4a7d7
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2011-03-03 21:21:08 +0000

    With threading macro.

--- src/clojure/multicore/pnehm/clojure_porderer.clj
+++ src/clojure/multicore/pnehm/clojure_porderer.clj
@@ -12,8 +12,9 @@
     (if-not (zero? freq) freq (compare val1 val2))))
 
 (defn pfrequency-map [coll]
-  (let [l (count coll)
-        partitioned-coll (partition-all (quot l cores) coll)
+  (let [partitioned-coll (-> (count coll)
+    (quot cores)
+    (partition-all coll))
         parts (pmap frequencies partitioned-coll)]
     (sort cmpr
       (apply merge-with + parts))))
