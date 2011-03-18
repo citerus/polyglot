d6150c5dea5877b3515b072a8117091a689fc7c5
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2011-03-18 22:55:04 +0100

    Fixed bug in test and impl of vec-part.

--- src/clojure/multicore/pnehm/clojure_porderer.clj
+++ src/clojure/multicore/pnehm/clojure_porderer.clj
@@ -1,4 +1,5 @@
 (ns multicore.pnehm.clojure-porderer
+  (:use [clojure.contrib.math :only (ceil)])
   (:gen-class
     :name multicore.pnehm.ClojureOrderer
     :implements [pnehm.Orderer]))
@@ -6,28 +7,27 @@
 (def cores (.. Runtime getRuntime availableProcessors))
 
 (defn vec-part [n coll]
-  (let [size-ix (- (count coll) 1)]
+  "Partition the given column into chunks of size n. Last chunk may contain less than n items"
+  (let [size-ix (count coll)]
     (loop [result [] i 0]
-      (if (>= i size-ix)
-        result
-        (recur (conj result (subvec coll i (+ i n))) (+ i n))))))
+      (cond
+        (= i size-ix) result
+        (> (+ i n) size-ix) (conj result (subvec coll i size-ix))
+        :else (recur (conj result (subvec coll i (+ i n))) (+ i n))))))
+
+(defn part-size [coll]
+  "Calculate the chunck size based on number of available cores"
+  (ceil (/ (count coll) cores)))
 
 (defn cmpr [[val1 freq1] [val2 freq2]]
   (let [freq (compare freq2 freq1)]
     (if-not (zero? freq) freq (compare val1 val2))))
 
 (defn pfrequency-map [coll]
-  (let [partitioned-coll (time (doall (-> (count coll) (quot cores) (vec-part coll))))
+  (let [partitioned-coll (time (doall (vec-part (part-size coll) coll)))
         parts (time (doall (pmap frequencies partitioned-coll)))]
     (time (sort cmpr
       (apply merge-with + parts)))))
 
 (defn -orderByFreq [_ coll]
-  (if (empty? coll) () (keys (pfrequency-map coll))))
-
-(defn partition-in [parts coll]
-  (let [part-size (/ (count coll) parts)]
-    (loop [e [] c coll]
-      (if (empty? c)
-        e
-        (recur (conj e (take part-size c)) (drop part-size c))))))
+  (if (empty? coll) () (keys (pfrequency-map (vec coll)))))
--- testsrc/clojure/multicore/pnehm/clojure_porderer_test.clj
+++ testsrc/clojure/multicore/pnehm/clojure_porderer_test.clj
@@ -2,7 +2,7 @@
   (:use multicore.pnehm.clojure-porderer clojure.test))
 
 (deftest test-order-by-freq-1
-  (is (= ["b","a","c","d"] (-orderByFreq :a ["b", "d", "b", "b", "a", "c", "a"]))))
+ (is (= ["b","a","c","d"] (-orderByFreq :a ["b", "d", "b", "b", "a", "c", "a"]))))
 
 (deftest test-order-by-freq-2
   (is (= ["a","b","d","c", "e"] (-orderByFreq :a ["b", "d", "d", "e", "b", "a", "c", "a"]))))
@@ -11,8 +11,9 @@
   (is (empty? (-orderByFreq :a []))))
 
 (deftest test-vec-part
-  (is [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]] (vec-part 3 ["a", "b", "c", "d", "e", "f", "g", "h", "i"]))
-  (is [["a", "b", "c", "d"], ["e", "f", "g", "h"], ["i"]] (vec-part 4 ["a", "b", "c", "d", "e", "f", "g", "h", "i"]))
-  (is [["a", "b", "c", "d", "e", "f", "g", "h", "i"]] (vec-part 9 ["a", "b", "c", "d", "e", "f", "g", "h", "i"])))
+  (is (= [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]] (vec-part 3 ["a", "b", "c", "d", "e", "f", "g", "h", "i"])))
+  (is (= [["a", "b", "c", "d"], ["e", "f", "g", "h"], ["i"]] (vec-part 4 ["a", "b", "c", "d", "e", "f", "g", "h", "i"])))
+  (is (= [["a", "b", "c", "d", "e", "f", "g", "h", "i"]] (vec-part 9 ["a", "b", "c", "d", "e", "f", "g", "h", "i"])))
+  (is (= [["b"], ["d"], ["d"], ["e"], ["b"], ["a"], ["c"], ["a"]] (vec-part 1 ["b", "d", "d", "e", "b", "a", "c", "a"]))))
 
 (run-tests 'multicore.pnehm.clojure-porderer-test)
